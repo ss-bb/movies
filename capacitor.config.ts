e838c6c86191c74d434d75446a1f5a109b956631
olivier <olivier.goyon@ocus.com>
2024-07-09 22:06:09 +0200

    feat(main): #0 just push capacitor stuff

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'zatsit.com',
+  appName: 'zatsit',
+  webDir: 'www'
+};
+
+export default config;
